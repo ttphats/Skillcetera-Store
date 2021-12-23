9bf77e5becb0992117226a0f189efd056d2aa2b1
ttphats <skphat789@gmail.com>
2021-12-23 19:42:26 +0700

    updated commit

--- assets/js/closeLoginBox.js
+++ assets/js/closeLoginBox.js
@@ -1,4 +1,4 @@
 function closeLoginBox() {
-    document.getElementById("img1").style.zIndex = "1";
+    document.getElementById("login-box").style.zIndex = "0";
     document.getElementById('modal__container').style.display = "none";
 }
--- assets/js/openLoginBox.js
+++ assets/js/openLoginBox.js
@@ -1,4 +1,3 @@
 function openLoginBox() {
-    document.getElementById("login-box").style.zIndex = "1";
     document.getElementById('modal__container').style.display = "flex";
 }
