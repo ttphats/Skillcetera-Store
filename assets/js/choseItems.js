2ffa0d19e7cb574043dafa02009b478b6ab01c02
ttphats <skphat789@gmail.com>
2021-12-23 23:31:36 +0700

    second

--- assets/js/choseItems.js
+++ assets/js/choseItems.js
@@ -1,28 +1,28 @@
 function choseItems(number) {
     if (number == 1) {
         document.getElementById("firstChoseItem").style.backgroundColor = "var(--primary-color)";
-        document.getElementById("secondChoseItem").style.backgroundColor = "white";
-        document.getElementById("thirdChoseItem").style.backgroundColor = "white";
-        document.getElementById("fourthChoseItem").style.backgroundColor = "white";
+        document.getElementById("secondChoseItem").style.backgroundColor = "#f3f3f3";
+        document.getElementById("thirdChoseItem").style.backgroundColor = "#f3f3f3";
+        document.getElementById("fourthChoseItem").style.backgroundColor = "#f3f3f3";
     }
 
     if (number == 2) {
-        document.getElementById("firstChoseItem").style.backgroundColor = "white";
+        document.getElementById("firstChoseItem").style.backgroundColor = "#f3f3f3";
         document.getElementById("secondChoseItem").style.backgroundColor = "var(--primary-color)";
-        document.getElementById("thirdChoseItem").style.backgroundColor = "white";
-        document.getElementById("fourthChoseItem").style.backgroundColor = "white";
+        document.getElementById("thirdChoseItem").style.backgroundColor = "#f3f3f3";
+        document.getElementById("fourthChoseItem").style.backgroundColor = "#f3f3f3";
     }
 
     if (number == 3) {
-        document.getElementById("firstChoseItem").style.backgroundColor = "white";
-        document.getElementById("secondChoseItem").style.backgroundColor = "white";
+        document.getElementById("firstChoseItem").style.backgroundColor = "#f3f3f3";
+        document.getElementById("secondChoseItem").style.backgroundColor = "#f3f3f3";
         document.getElementById("thirdChoseItem").style.backgroundColor = "var(--primary-color)";
-        document.getElementById("fourthChoseItem").style.backgroundColor = "white";
+        document.getElementById("fourthChoseItem").style.backgroundColor = "#f3f3f3";
     }
     if (number == 4) {
-        document.getElementById("firstChoseItem").style.backgroundColor = "white";
-        document.getElementById("secondChoseItem").style.backgroundColor = "white";
-        document.getElementById("thirdChoseItem").style.backgroundColor = "white";
+        document.getElementById("firstChoseItem").style.backgroundColor = "#f3f3f3";
+        document.getElementById("secondChoseItem").style.backgroundColor = "#f3f3f3";
+        document.getElementById("thirdChoseItem").style.backgroundColor = "#f3f3f3";
         document.getElementById("fourthChoseItem").style.backgroundColor = "var(--primary-color)";
     }
 }
